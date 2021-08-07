388becef7e391f2b4afd6a884de58b1827e33275
Denny C. Dai <dennycd@me.com>
2021-08-07 01:39:17 -0700

    objc pr test (#2)
    * objc tutorial test
    
    * additional update

--- examples/objective-c/helloworld/main.m
+++ examples/objective-c/helloworld/main.m
@@ -59,9 +59,15 @@ int main(int argc, char * argv[]) {
     options.transport = GRPCDefaultTransportImplList.core_insecure;
     options.userAgentPrefix = @"HelloWorld/1.0";
 
+    NSLog("requesting hello");
     [[client sayHelloWithMessage:request
                  responseHandler:[[HLWResponseHandler alloc] init]
                      callOptions:options] start];
+      
+    NSLog("requesting hello again");
+    [[client sayHelloAgainWithMessage:request
+                        responseHandler:[[HLWResponseHandler alloc] init]
+                            callOptions:options] start];
 
     return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
   }
